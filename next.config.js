c41c30df499d585e39670691b697ed6541020f87
Lin Zhang <yabao.0x5a@gmail.com>
2021-12-13 06:21:24 -0800

    add ignoreBuildErrors

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
-module.exports = {};
+module.exports = {
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
+  }
